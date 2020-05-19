35732ef9d934c795d3f612b2f99351e56d5f38a1
rurito <rurito.blog@gmail.com>
2020-05-19 18:34:13 +0900

    Playerの親の順番を決める部分を実装

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    private List<int> menber_turn_list;
+    private List<BasePlayer> menber_list;
+
+    private List<string> player_names = new List<string>() { "AaA", "BbB", "CcC", "DdD" };
+
     void Start()
     {
-
         // 親と順番を決める
-        
+        menber_turn_list = ShuffleNumber(4);
+
+        // Playerを初期化する
+        for( int i = 0; i < 4; i++ )
+        {
+            DefaultPlayer tmp_player = new DefaultPlayer();
+            tmp_player.Setup(menber_turn_list[i], player_names[i]);
+            menber_list.Add( tmp_player );
+        }
+        Main();
     }
 
     // メイン関数
     void Main()
     {
-
         // 牌を並べる部分
 
         
@@ -34,4 +46,24 @@ public class GameController : MonoBehaviour
 
     }
 
+    // 順番をランダムに決める
+    private List<int> ShuffleNumber(int key)
+    {
+        List<int> list = new List<int>();
+
+        for( int i = 0; i < key; i++ )
+        {
+            list.Add(i + 1);
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int tmp = list[i];
+            int randomIndex = Random.Range(0, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = tmp;
+        }
+
+        return list;
+    }
 }
