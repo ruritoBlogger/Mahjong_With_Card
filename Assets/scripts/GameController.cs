f9b887895ef4b03adbd581510cb5c30a0e8dcfb8
rurito <rurito.blog@gmail.com>
2020-05-19 11:00:36 +0900

    ファイル群を生成

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // メイン関数
+    void Main()
+    {
+
+    }
+
+}
