01be160d11a3583d5e3d86c54b1d58845bde8e82
rurito <rurito.blog@gmail.com>
2020-05-19 13:40:35 +0900

    ランダムに牌を切るAI?を実装した

---
+++ Assets/scripts/DefaultPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultPlayer : BasePlayer
+{
+    // ランダムに切る牌を選ぶ
+    public override int ChoicePai()
+    {
+        int key = (int)Random.Range(0f, 14f);
+        return key;
+    }
+}
