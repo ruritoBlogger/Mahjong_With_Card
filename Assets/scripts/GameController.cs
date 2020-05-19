da5bde28a2aabb97aeb94f25eb6dd3f221a65819
rurito <rurito.blog@gmail.com>
2020-05-19 11:01:07 +0900

    麻雀の軸関数のテンプレを作成した

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -6,12 +6,31 @@ public class GameController : MonoBehaviour
 {
     void Start()
     {
+
+        // 親と順番を決める
         
     }
 
     // メイン関数
     void Main()
     {
+
+        // 牌を並べる部分
+
+        
+        // ドラチェック
+
+
+        // 実際に打つ部分
+
+
+        // 上がりチェック
+
+
+        // もし流局すれば流局処理
+
+
+        // 順位処理
 
     }
 
