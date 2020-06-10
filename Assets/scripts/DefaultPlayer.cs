6d371e14c0343f88fcf3e07089b38daef88e60a8
rurito <rurito.blog@gmail.com>
2020-06-11 00:14:43 +0900

    プレイヤーが捨てる牌を選択する部分に遅延処理を挟んだ

--- Assets/scripts/DefaultPlayer.cs
+++ Assets/scripts/DefaultPlayer.cs
@@ -8,6 +8,12 @@ public class DefaultPlayer : BasePlayer
     public override int ChoicePai()
     {
         int key = (int)Random.Range(0f, 13f);
+        StartCoroutine(WaitChoicingPai());
         return key;
+    }
+
+    IEnumerator WaitChoicingPai()
+    {
+        yield return new WaitForSeconds(3);
     }
 }
