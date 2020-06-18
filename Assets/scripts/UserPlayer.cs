10c7d1f9c17457da7265cd86e956724ea4d5d3cc
rurito <rurito.blog@gmail.com>
2020-06-18 22:15:34 +0900

    プレイヤーが捨てる牌を選択できるコードを実装

---
+++ Assets/scripts/UserPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserPlayer : BasePlayer
+{
+    GameObject clickedGameObject;
+
+    // クリックを検知する
+    private void Update()
+    {
+        if( Input.GetMouseButtonDown(0))
+        {
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if( Physics.Raycast(ray, out hit))
+            {
+                clickedGameObject = hit.collider.gameObject;
+            }
+            
+        }
+        else
+        {
+            clickedGameObject = null;
+        }
+    }
+
+
+    public override int ChoicePai()
+    {
+        while ( clickedGameObject == null )
+        {
+        }
+        Debug.Log((int)(HandsPosition.x - clickedGameObject.transform.position.x));
+        return (int)(HandsPosition.x - clickedGameObject.transform.position.x);
+    }
+}
